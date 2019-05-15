bd92d48324d78b68a1055219ca13504d27529793
zichuan wei <zichuan@live.com>
2019-05-15 14:46:05 -0400

    challenge 3 done

--- set1/cha3.py
+++ set1/cha3.py
@@ -1,7 +1,8 @@
 import cha1
+import sys
 
 #the source text is encoded by XOR a repeating single character
-#as such, there is only 2556 possibilities, and we can just
+#as such, there is only 256 possibilities, and we can just
 #output them all and find the suitable one
 
 #s is the input string, and c is the character we decode against
@@ -9,8 +10,12 @@ def decode(s , c):
 	a = cha1.hex_string_to_bytes(s)
 	b = bytearray(a)
 	for i in range(len(a)):
-		b[i] = a[i] ^ c
+		b[i] = a[i] ^c 
 	return b
 		
 if "__main__":
-	pass
+	s = sys.argv[1]
+	for c in range(256):
+		print(str(decode(s, c)))
+
+#the answer is "b"ooking MC\'s like a pound of bacon""
